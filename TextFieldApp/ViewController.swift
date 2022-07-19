25d42c01475616bd78f0e59e9df0e1b6655ca388
pololover <pololover>
2022-07-19 17:15:38 +0900

    FEAT: 텍스트필드 연결 및 스타일, 벨리데이션 구조 정의

--- TextFieldApp/ViewController.swift
+++ TextFieldApp/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.delegate = self
+        setup()
+        
+    }
+    
+    func setup() {
+        view.backgroundColor = UIColor.gray
+        textField.keyboardType = .emailAddress
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "이메일 입력"
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .next
+    }
+    
+    // 텍스트 입력을 시작할때 (시작할지 말지)
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    // 텍스트 입력할 때 시점
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    
+    // 텍스트를 지울지 말지
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    // 한글자씩 입력받을 때 실행하는 함수 (입력받을지 말지)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        if string == "a" {
+            return false
+        }else {
+            return true
+        }
+    }
+    
+    // 리턴 버튼클릭시 리턴할지 말지
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    // 텍스트 입력을 끝낼 때 호출(끝낼지 말지)
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
     }
 
-
+    // 텍스트필드가 실제 끝났을 때의 시점
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(#function)
+    }
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
